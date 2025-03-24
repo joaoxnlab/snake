97ad759a7eba497e55c9daff880ad67d3ae786e2
João Olivo <jvsoxg@gmail.com>
2025-03-24 09:39:05 -0300

    cobra aumenta de tamanho

--- out/script.js
+++ out/script.js
@@ -37,22 +37,38 @@ class Entidade {
     }
 }
 class Cobra extends Entidade {
+    velocidade = 5;
     cor = '#ffa';
+    segmentos = [];
     constructor(x, y, largura, altura) {
         super(x, y, largura, altura);
+        this.segmentos.push({ x, y });
     }
     atualizar() {
+        // Move cada segmento para a posição do segmento à frente
+        for (let i = this.segmentos.length - 1; i > 0; i--) {
+            this.segmentos[i] = { ...this.segmentos[i - 1] };
+        }
+        // Atualiza a posição da cabeça da cobra
         if (teclasPressionadas.KeyW) {
-            this.y -= 7;
+            this.y -= this.velocidade;
         }
         else if (teclasPressionadas.KeyS) {
-            this.y += 7;
+            this.y += this.velocidade;
         }
         else if (teclasPressionadas.KeyA) {
-            this.x -= 7;
+            this.x -= this.velocidade;
         }
         else if (teclasPressionadas.KeyD) {
-            this.x += 7;
+            this.x += this.velocidade;
+        }
+        // Atualiza a posição do primeiro segmento para a nova posição da cabeça
+        this.segmentos[0] = { x: this.x, y: this.y };
+    }
+    desenhar() {
+        ctx.fillStyle = this.cor ?? 'black';
+        for (const segmento of this.segmentos) {
+            ctx.fillRect(segmento.x, segmento.y, this.largura, this.altura);
         }
     }
     verificarColisao(comida) {
@@ -65,8 +81,11 @@ class Cobra extends Entidade {
     }
     #houveColisao(comida) {
         pontuacao++;
-        comida.x = Math.random() * canvas.width - comida.largura;
-        comida.y = Math.random() * canvas.height - comida.altura;
+        comida.x = Math.random() * (canvas.width - comida.largura);
+        comida.y = Math.random() * (canvas.height - comida.altura);
+        // Adiciona um novo segmento no final da cobra
+        const ultimoSegmento = this.segmentos[this.segmentos.length - 1];
+        this.segmentos.push({ x: ultimoSegmento.x, y: ultimoSegmento.y });
     }
     dentroDoCanvas() {
         return this.x >= 0 &&
@@ -78,15 +97,15 @@ class Cobra extends Entidade {
 class Comida extends Entidade {
     cor = '#f33';
     constructor() {
-        super(Math.random() * canvas.width - 20, Math.random() * canvas.height - 20, 20, 20);
+        super(Math.random() * (canvas.width - 20), Math.random() * (canvas.height - 20), 20, 20);
     }
 }
 const cobra = new Cobra(100, 200, 20, 20);
 const comida = new Comida();
 function gameOver() {
     ctx.fillStyle = 'white';
-    ctx.font = 'bold 20px Arial';
-    ctx.fillText(`Fim de jogo! Pontuação final: ${pontuacao}`, 10, 30);
+    ctx.font = 'bold 30px Arial';
+    ctx.fillText(`Fim de jogo! Pontuação final: ${pontuacao}`, 10, 40);
 }
 function escreverPontuacao() {
     ctx.fillStyle = 'white';
--- src/script.ts
+++ src/script.ts
@@ -45,20 +45,40 @@ class Entidade {
 
 
 class Cobra extends Entidade {
+    readonly velocidade = 5;
     cor = '#ffa';
+    segmentos: { x: number, y: number }[] = [];
 
     constructor(x: number, y: number, largura: number, altura: number) {
         super(x, y, largura, altura);
+        this.segmentos.push({ x, y });
     }
+
     atualizar() {
+        // Move cada segmento para a posição do segmento à frente
+        for (let i = this.segmentos.length - 1; i > 0; i--) {
+            this.segmentos[i] = { ...this.segmentos[i - 1] };
+        }
+
+        // Atualiza a posição da cabeça da cobra
         if (teclasPressionadas.KeyW) {
-            this.y -= 7;
+            this.y -= this.velocidade;
         } else if (teclasPressionadas.KeyS) {
-            this.y += 7;
+            this.y += this.velocidade;
         } else if (teclasPressionadas.KeyA) {
-            this.x -= 7;
+            this.x -= this.velocidade;
         } else if (teclasPressionadas.KeyD) {
-            this.x += 7;
+            this.x += this.velocidade;
+        }
+
+        // Atualiza a posição do primeiro segmento para a nova posição da cabeça
+        this.segmentos[0] = { x: this.x, y: this.y };
+    }
+
+    desenhar() {
+        ctx.fillStyle = this.cor ?? 'black';
+        for (const segmento of this.segmentos) {
+            ctx.fillRect(segmento.x, segmento.y, this.largura, this.altura);
         }
     }
     verificarColisao(comida: Entidade) {
@@ -71,10 +91,15 @@ class Cobra extends Entidade {
             this.#houveColisao(comida);
         }
     }
+
     #houveColisao(comida: Entidade) {
         pontuacao++;
-        comida.x = Math.random() * canvas.width - comida.largura;
-        comida.y = Math.random() * canvas.height - comida.altura;
+        comida.x = Math.random() * (canvas.width - comida.largura);
+        comida.y = Math.random() * (canvas.height - comida.altura);
+
+        // Adiciona um novo segmento no final da cobra
+        const ultimoSegmento = this.segmentos[this.segmentos.length - 1];
+        this.segmentos.push({ x: ultimoSegmento.x, y: ultimoSegmento.y });
     }
 
     dentroDoCanvas(): boolean {
@@ -84,10 +109,11 @@ class Cobra extends Entidade {
             this.y + this.altura <= canvas.height;
     }
 }
+
 class Comida extends Entidade {
     cor = '#f33';
     constructor() {
-        super(Math.random() * canvas.width - 20, Math.random() * canvas.height - 20, 20, 20);
+        super(Math.random() * (canvas.width - 20), Math.random() * (canvas.height - 20), 20, 20);
     }
 }
 
@@ -97,8 +123,8 @@ const comida = new Comida();
 
 function gameOver() {
     ctx.fillStyle = 'white';
-    ctx.font = 'bold 20px Arial';
-    ctx.fillText(`Fim de jogo! Pontuação final: ${pontuacao}`, 10, 30);
+    ctx.font = 'bold 30px Arial';
+    ctx.fillText(`Fim de jogo! Pontuação final: ${pontuacao}`, 10, 40);
 }
 
 function escreverPontuacao() {
